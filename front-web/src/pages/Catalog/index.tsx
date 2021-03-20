deeedbc15d8e8c1fc21aa2bf02b15b28724d84cb
Estevao Souza <estevaosouza18@gmail.com>
2021-03-20 16:40:29 -0300

    Add product listing with backend

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,23 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ProductsResponse } from '../../core/types/Products';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 
 const Catalog = () => {
-  //quando a lista de produtos estiver disponível, 
-  //popular um estado no componente e listar os produtos dinamicamente
-
-  const params = {
-    page: 0,
-    linesPerPage: 12
-  }
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
   
-  //quando o componente iniciar, buscar a lista de produtos
   useEffect(() => {
-    //requisiçao pro backend
+    const params = {
+      page: 0,
+      linesPerPage: 12
+    }
+
     makeRequest({url: '/products', params})
-      .then(response => console.log(response));
+      .then(response => setProductsResponse(response.data));
   }, [])
 
   return (
@@ -26,15 +24,11 @@ const Catalog = () => {
         Catálogo de produtos
       </h1>
       <div className="catalog-products">
-        <Link to="/products/1"><ProductCard /></Link>
-        <Link to="/products/2"><ProductCard /></Link>
-        <Link to="/products/3"><ProductCard /></Link>
-        <Link to="/products/4"><ProductCard /></Link>
-        <Link to="/products/5"><ProductCard /></Link>
-        <Link to="/products/6"><ProductCard /></Link>
-        <Link to="/products/7"><ProductCard /></Link>
-        <Link to="/products/8"><ProductCard /></Link>
-        <Link to="/products/9"><ProductCard /></Link>
+        {productsResponse?.content.map(product => (
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <ProductCard product={product}/>
+          </Link>
+        ))}
       </div>
     </div>
   );
